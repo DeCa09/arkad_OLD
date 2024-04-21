4cae288613c81d4c31e82d65226081f9165130a9
Demir Catovic <dcatovic@amazon.com>
2024-04-21 17:23:56 +0200

    add retrieval func to extract body of the jsonresponse

--- sec/src/ingestion/mod.rs
+++ sec/src/ingestion/mod.rs
@@ -3,11 +3,12 @@ pub mod storage;
 
 use std::error::Error;
 
-use retrieval::retrieve_document;
+use retrieval::run_retrieval_process;
 use storage::store_to_disk;
 
+
 pub async fn ingest_document() -> Result<(), Box<dyn Error>> {
-    retrieve_document().await?;
+    run_retrieval_process().await?;
     store_to_disk();
     Ok(())
 }
--- sec/src/ingestion/retrieval/mod.rs
+++ sec/src/ingestion/retrieval/mod.rs
@@ -1,15 +1,29 @@
-use std::error::Error;
+use reqwest::Error;
 
 use config::{get_ciks_to_parse, get_sec_user_client};
 
 mod config;
 
-pub async fn retrieve_document() -> Result<(), Box<dyn Error>> {
-    println!("User Client: {:?}", get_sec_user_client());
-    println!("CIKs: {:?}", get_ciks_to_parse());
-    println!("I retrieved something!");
 
-    // println!("Will look for this company: {cik}");
+pub async fn retrieve_document(cik: &str) -> Result<String, Error> {
+    let url = format!("https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json");
+    
+    let client = get_sec_user_client()?;
+
+    let body = client.get(&url).send().await?.text().await?;
+
+    println!("Did the retrieval process for this cik: {cik} with this body: {}...", &body[..100]);
+
+    Ok(body)
+}
+
+pub async fn run_retrieval_process() -> Result<(), Error>{
+    let ciks = get_ciks_to_parse();
+
+    for cik in ciks {
+        retrieve_document(cik).await?;
+    }
 
     Ok(())
+
 }
