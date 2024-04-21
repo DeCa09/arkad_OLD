9dfb880c22201ead4fcafb2e8862287c78ab5949
Demir Catovic <dcatovic@amazon.com>
2024-04-21 15:22:14 +0200

    add stuff to enable compilation without warnings

--- sec/src/ingestion/mod.rs
+++ sec/src/ingestion/mod.rs
@@ -1,3 +1,9 @@
+pub mod retrieval;
+
+
+use retrieval::get_user_agent;
+
 pub fn retrieve_documents(){
     println!("I retrieved something!");
+    println!("Here is the User Agent we use for retrieval: {}", get_user_agent());
 }
